8d67875b5d8047fdc76d1c416f0861f9bf384951
Ani <eradzeani10@gmail.com>
2024-09-30 21:19:14 +0400

    Task 2 homework

---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System.Linq;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+List<TaskItem> taskItems = new()
+{
+    new TaskItem(1, "Complete Assignment", "Important", new DateTime(2024, 9, 30)),
+    new TaskItem(2, "Grocery Shopping", "Chore", new DateTime(2024, 10, 2)),
+    new TaskItem(3, "Finish Project", "Work", new DateTime(2024, 10, 5)),
+};
+
+app.MapGet("/tasks", () => Results.Ok(taskItems));
+
+
+app.MapGet("/task/{id:int}", (int id) =>
+{
+    var task = taskItems.SingleOrDefault(t => t.Id == id);
+    return task is not null ? Results.Ok(task) : Results.NotFound($"Task with ID {id} not found");
+});
+
+
+app.Run();
+
+public record TaskItem(int Id, string Title, string Category, DateTime DueDate);
